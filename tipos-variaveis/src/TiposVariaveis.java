d2aa36dc3285a7c4f252cac0bcccff4ab088e705
George Bergson <george_bergson@hotmail.com>
2024-07-15 13:31:35 -0300

    Tipos e Constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       double salarioMinimo = 2500.33;
+       int valor1 = 5;
+       valor1 = 10;
+        System.out.println( salarioMinimo);
+        System.out.println(valor1);
+
+        final double PI = 3.14;
+        //PI = 3.15; // não pode mudar o valor de uma variável
+        System.out.println(PI);
     }
 }
